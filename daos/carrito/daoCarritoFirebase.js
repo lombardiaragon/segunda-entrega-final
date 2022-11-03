e5af704efd79585b5112c3f134a336cc3962768e
Federico Lombardi <federicolombardi_10@hotmail.com>
2022-11-03 18:59:18 -0300

    s

--- daos/carrito/daoCarritoFirebase.js
+++ daos/carrito/daoCarritoFirebase.js
@@ -4,16 +4,16 @@ class DaoCarritoFirebase extends ContenedorFirebase{
     constructor(){
         super("carritos",{
             timesTamp:Date,
-            productos:{type:[], required:true, default:[]}
+            productos:{type:[], required:true, default:[]} //no me lo crea por defecto
         })
     }
     // no me saleeee
     async saveNewProd(id,newProd){
         const querySnapshot=await super.findAll()
-        const data=querySnapshot.find(el=>el.id==id)
-        // querySnapshot.forEach(doc=>console.log(doc.data()))
- 
-        
+        let data=querySnapshot.find(el=>el.id==id)
+
+        await this.query.doc(id).update({productos:newArray})
+
         return console.log('carritossssss',data.data());
     }
 }
